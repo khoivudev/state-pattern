285d168d43a1c4f834a61463d58925bf96619a58
khoivudev <vuhuykhoi1997@gmail.com>
2021-11-02 12:10:10 +0900

    [feat] Implement state for gumball machine

---
+++ README.md
@@ -0,0 +1,3 @@
+**自動販売機**
+## IDEA
+![alt_image](/design/idea.png)
---
+++ src/GumballMachine/GumballMachine.java
@@ -0,0 +1,75 @@
+package GumballMachine;
+
+import State.*;
+
+public class GumballMachine {
+    State soldOutState;
+    State noQuarterState;
+    State hasQuarterState;
+    State soldState;
+
+    State state;                    // Init state of the machine.
+    int count = 0;                  // Numbers of gumballs in the machine.
+
+    public GumballMachine (int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+
+        this.count = numberGumballs;
+        if (numberGumballs > 0) {
+            state = noQuarterState;
+        } else {
+            state = soldOutState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+       state.turnCrank();
+       state.dispense();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void dispense() {
+        state.dispense();
+    }
+
+    public void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count = count -1;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+}
